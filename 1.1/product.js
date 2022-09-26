8a4d038d2bce285dc8624b36f578caff4e161370
Marina-Ogirenko <49350387+Marina-Ogirenko@users.noreply.github.com>
2022-09-27 00:44:15 +0300

    Add files via upload

--- 1.1/product.js
+++ 1.1/product.js
@@ -1,5 +1,5 @@
 function Product(productID, productName, productDescription, productPrice,
-  productBrand, productQuantity, productActiveSize, productDate) {
+  productBrand, productQuantity, productActiveSize, productDate, pathImg) {
   this.ID;
   this.description;
   this.name;
@@ -13,49 +13,54 @@ function Product(productID, productName, productDescription, productPrice,
   this.images = [];
 
   this.setImages = function (pathImg) {
-    var img = new Image();
-    img.src = pathImg;
-    this.images.push(img);
+    this.images.push(pathImg);
   }
 
   this.getImage = function (numberImg) {
-    if (numberImg = undefined) {
+    if (numberImg == undefined) {
       return (this.images[0])
     }
 
-    if (numberImg <= this.images.length) {
+    if (numberImg < this.images.length) {
       return (this.images[numberImg])
     }
     console.log("Картинки с таким ключем не существует");
+  }
+
+  if(pathImg !=undefined) {
+    this.setImages(pathImg);
   }
 
   this.setID = function (productID) {
     this.ID = productID;
   }
 
-  this.setID(productID);
+  if(productID!=undefined) {
+    this.setID(productID);
+  }
 
   this.getID = function () {
     return this.ID;
   }
 
   this.setName = function (productName) {
-    console.log(`Hello, I'm ${productName}`)
 
     if (typeof (productName) != "string") {
-      console.log("Введено число, введи имя!");
+      console.log("Введено число, введи имя field Name!");
       return;
     }
 
     if (productName.length < 3) {
-      console.log("Имя слишком короткое, должно быть более 3 символов");
+      console.log("Имя слишком короткое, должно быть более 3 символов field Name");
       return;
     }
 
     this.name = productName;
   };
 
-  this.setName(productName);
+  if(productName!=undefined) {
+    this.setName(productName);
+  }
 
   this.getName = function () {
     return this.name;
@@ -63,13 +68,15 @@ function Product(productID, productName, productDescription, productPrice,
 
   this.setDescription = function (productDescription) {
     if (productDescription.length < 5) {
-      console.log("Имя слишком короткое, должно быть более 5 символов");
+      console.log("Имя слишком короткое, должно быть более 5 символов field Description");
       return;
     }
     this.description = productDescription;
   };
 
+  if(productDescription!=undefined) {
   this.setDescription(productDescription);
+  }
 
   this.getDescription = function () {
     return this.description;
@@ -77,19 +84,21 @@ function Product(productID, productName, productDescription, productPrice,
 
   this.setPrice = function (productPrice) {
     if (typeof (productPrice) != 'number') {
-      console.log("Должны быть цифры!");
+      console.log("Должны быть цифры! field Price");
       return;
     }
 
     if (productPrice < 0 && productPrice > 10000) {
-      console.log("Некорректное значение!");
+      console.log("Некорректное значение! field Price");
       return;
     }
 
     this.price = productPrice;
   }
 
-  this.setPrice(productPrice);
+  if(productPrice!=undefined) {
+    this.setPrice(productPrice);
+  }
 
   this.getPrice = function () {
     return this.price;
@@ -98,19 +107,21 @@ function Product(productID, productName, productDescription, productPrice,
   this.setBrand = function (productBrand) {
 
     if (typeof (productBrand) != "string") {
-      console.log("Введено число, введи имя!");
+      console.log("Введено число, введи имя! field Brand");
       return;
     }
 
     if (productBrand.length < 3) {
-      console.log("Имя слишком короткое, должно быть более 3 символов");
+      console.log("Имя слишком короткое, должно быть более 3 символов. field Brand");
       return;
     }
 
     this.brand = productBrand;
   };
 
+  if(productBrand!=undefined) {
   this.setBrand(productBrand);
+  }
 
   this.getBrand = function () {
     return this.brand;
@@ -118,12 +129,12 @@ function Product(productID, productName, productDescription, productPrice,
 
   this.setQuantity = function (productQuantity) {
     if (typeof (productQuantity) != 'number') {
-      console.log("Должны быть цифры!");
+      console.log("Должны быть цифры! Field Quantity");
       return;
     }
 
     if (productQuantity < 0 && productQuantity > 10000) {
-      console.log("Некорректное значение!");
+      console.log("Некорректное значение! Field Quantity");
       return;
     }
 
@@ -134,7 +145,9 @@ function Product(productID, productName, productDescription, productPrice,
     return this.quantity;
   }
 
-  this.setQuantity(productQuantity);
+  if(productQuantity!=undefined){
+    this.setQuantity(productQuantity);
+  }
 
   this.setActiveSize = function (productActiveSize) {
 
@@ -146,30 +159,38 @@ function Product(productID, productName, productDescription, productPrice,
     }
 
     if (this.activeSize != productActiveSize) {
-      console.log("Такого значения не существует!");
+      console.log("Такого значения не существует! Field activeSize");
       return;
     }
   };
 
+  if(productActiveSize!=undefined) {
   this.setActiveSize(productActiveSize);
+  }
 
   this.getActiveSize = function () {
     return this.activeSize;
   };
 
+this.getSizes = function() {
+  return this.sizes;
+}
+
   this.setDate = function (productDate) {
-    if (Date.parse(productDate) != Nan) {
+    if (Date.parse(productDate) != NaN) {
       this.date = new Date(Date.parse(productDate));
     }
     else
-      console.log("Введи дату в формате 'YYYY-MM-DDTHH:mm:ss.sss'");
+      console.log("Введи дату в формате 'YYYY-MM-DDTHH:mm:ss.sss' field Date");
   };
 
   this.getDate = function () {
     return this.date;
   };
 
+  if(productDate!=undefined) {
   this.setDate(productDate);
+  }
 
   function Reviews(ID, date, comment, author, keyRating, valueRating) {
     this.IDrew;
@@ -184,18 +205,20 @@ function Product(productID, productName, productDescription, productPrice,
     };
 
     this.setRating = function (keyRating, valueRating) {
-      if (typeof (valueRating) == Number || valueRating <= 0 || valueRating > 10) {
-        console.log("Некорректное значение!");
+      if ( (valueRating <=0 || valueRating >10)) {
+        console.log("Некорректное значение! Field Rating");
         return;
       }
-      if (rating.keyRating != undefined) {
-        rating.keyRating = valueRating;
+      if (this.rating.hasOwnProperty(keyRating)) {
+        this.rating[keyRating] = valueRating;
         return;
       }
-      console.log("Такого ключа нет!")
+      console.log("Такого ключа нет! Field Rating")
     }
 
-    this.setRating(keyRating, valueRating);
+    if(keyRating!=undefined && valueRating!=undefined) {
+      this.setRating(keyRating, valueRating);
+    }
 
     this.getRating = function () {
       return rating.keyRating;
@@ -205,7 +228,9 @@ function Product(productID, productName, productDescription, productPrice,
       this.IDrew = ID;
     }
 
-    this.setID(ID);
+    if(ID!=undefined){
+      this.setID(ID);
+    }
 
     this.getID = function () {
       return this.IDrew;
@@ -214,26 +239,28 @@ function Product(productID, productName, productDescription, productPrice,
     this.setAuthor = function (author) {
 
       if (typeof (author) != "string") {
-        console.log("Введено число, введи имя!");
+        console.log("Введено число, введи имя! Field Author");
         return;
       }
 
       if (author.length < 3) {
-        console.log("Имя слишком короткое, должно быть более 3 символов");
+        console.log("Имя слишком короткое, должно быть более 3 символов. Field Author");
         return;
       }
 
       this.author = author;
     };
 
+    if(author!=undefined){
     this.setAuthor(author);
+    }
 
     this.getAuthor = function () {
       return this.author;
     };
 
     this.setDate = function (date) {
-      if (Date.parse(date) != Nan) {
+      if (Date.parse(date) != NaN) {
         this.date = new Date(Date.parse(date));
       }
       else
@@ -244,17 +271,21 @@ function Product(productID, productName, productDescription, productPrice,
       return this.date;
     };
 
+    if(date!=undefined) {
     this.setDate(date);
+    }
 
     this.setComment = function (comment) {
       if (comment.length < 5) {
-        console.log("Имя слишком короткое, должно быть более 5 символов");
+        console.log("Имя слишком короткое, должно быть более 5 символов. Field Coment");
         return;
       }
       this.comment = comment;
     };
 
+    if(comment!=undefined){
     this.setComment(comment);
+    }
 
     this.getComment = function () {
       return this.comment;
@@ -273,59 +304,92 @@ function Product(productID, productName, productDescription, productPrice,
   this.deleteSize = function (delitedSize) {
     for (let i = 0; i < this.sizes.length; i++) {
       if (this.sizes[i] == delitedSize) {
-        this.sizes.slice(i, 1);
+        this.sizes.splice(i, 1);
         return;
       }
     }
-    console.log('Такого значения не существует!');
+    console.log('Такого значения не существует! deleteSize');
   }
 
 
   this.getReviewByID = function (getReviewID) {
     for (let i = 0; i < this.reviews.length; i++) {
-      if (this.reviews[i].IDrew = getReviewID) {
+      if (this.reviews[i].IDrew == getReviewID) {
         return this.reviews[i];
       }
     }
-    confirm.log("Такого ключа не существует!");
+    console.log("Такого ключа не существует! getReviewByID");
   }
 
   this.deleteReview = function (deleteRevID) {
     for (let i = 0; i < this.reviews.length; i++) {
-      if (this.reviews[i].IDrew = getReviewID) {
-        this.reviews.slice(i, 1);
+    
+      if (this.reviews[i].IDrew == deleteRevID) {
+        this.reviews.splice(i, 1);
+        return;
       }
     }
-    confirm.log("Такого ключа не существует!");
+    console.log("Такого ключа не существует! deleteReview");
   }
 
   this.getAverageRating = function () {
     let countRating = 0;
     let summRating = 0;
     for (let i = 0; i < this.reviews.length; i++) {
-      for (let key in this.reviews.rating) {
-        if (this.reviews.rating.key != null) {
-          summRating += this.reviews.rating.key;
+      for (let key in this.reviews[i].rating) {
+        if (this.reviews[i].rating[key] != null) {
+          summRating += this.reviews[i].rating[key];
           ++countRating;
         }
       }
 
     }
     if (summRating != 0) {
-      return summRating / countRating;
+      return summRating/countRating;
     }
     console.log('Отзывов нет!')
   }
 
+}
 
+function search(arrayProducts, searchingText ) {
+ for(let i = 0; i < arrayProducts.length; i++ ) {
+  let name = arrayProducts[i].getName();
+
+ }
 
 }
 
+let products = [
+  new Product(1,'T-shirt', 'sunset over the ocean',49, 'Kal', 10, 'S',10-10-2022, 
+  'C:\Users\Марина\Pictures\/t-shots.jpg'),
+  new Product(2,'ocean', 'helllo', 81, 'Kal', 8, 'S',10-12-2022, 
+  'C:\Users\Марина\Pictures\/t-shots.jpg'),
+  new Product(3,'sunset', 'helllo my ocean!', 32, 'Kal', 6, 'S',10-12-2022, 
+  'C:\Users\Марина\Pictures\/t-shots.jpg')
+];
 
-let oneProduct = new Product('Karl');
-// oneProduct.setName('Karell');
-oneProduct.setActiveSize('XS');
-console.log(oneProduct);
-console.log(oneProduct.getName());
+function search(products, ocean);
+
+
+
+let oneProduct = new Product(1,'T-shirt', 'sunset over the ocean',49, 'Kal', 10, 'S',10-10-2022, 
+'C:\Users\Марина\Pictures\/t-shots.jpg');
+oneProduct.addReview(1,10-09-2022,'Hello', 'abc', 'price', 10);
+oneProduct.addReview(2,10-07-2022,'Helloooo', 'avtor', 'value', 7);
+console.log(oneProduct.getImage());
+oneProduct.setImages('C:\Users\Марина\Pictures\/t.jpg');
+console.log(oneProduct.getImage(1));
+//oneProduct.addSize('MM');
+//console.log(oneProduct.getSizes());
+//oneProduct.deleteSize('MM');
+//console.log(oneProduct.getSizes());
+//console.log(oneProduct.getAverageRating());
+//oneProduct.deleteReview(2);
+//console.log(oneProduct.getReviewByID(2));
+//console.log('/n');
+//console.log(oneProduct.getReviewByID(1));
+//console.log(oneProduct);
+//console.log(oneProduct.getName());
 
 
